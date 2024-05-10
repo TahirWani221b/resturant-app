d73135ea55468d9640621c32cc0e89164353d61b
tahir.wani <tahir.waniiqbal@gmail.com>
2024-05-11 03:59:20 +0530

    food app cart

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './Modal.module.css'
+import ReactDOM from 'react-dom'
+
+const Backdrop = props => {
+    return <div className={`${classes.backdrop}`}></div>
+}
+
+const ModalOverlay = props => {
+    return (
+        <div className={`${classes.modal}`}>
+            <div className={`${classes.content}`}>{props.children}</div>
+        </div>
+    );
+}
+function Modal(props) {
+    const portalElement = document.getElementById('overlays');
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </>
+    )
+}
+
+export default Modal
